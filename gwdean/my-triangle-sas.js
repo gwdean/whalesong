d117f5687dbea34c1c86bd623c97c9fdbc42a6f5
Greg Dean <gwdean@gmail.com>
2011-10-17 16:15:11 -0400

    a few updates to triangle-sas.js and journal.org

--- gwdean/my-triangle-sas.js
+++ gwdean/my-triangle-sas.js
@@ -1,3 +1,37 @@
+I need to crack down and really figure out how these
+work well, so that I can pack them up and send them
+out soon.
+
+Things I need to finish off these files:
+
+* A copy of the kernel.js file so that I can figure out
+  how to reformat the images
+  
+* A conceptual description of how these files work. We
+  can use the triangle/sas as an example, and then see
+  what we can do for the others.
+  
+* Triangle/sas -> a conceptual description.
+
+(less than 90)
+Start at x,y
+Move along the x-axis to the length of side-c (I.e. 100 in our example)
+Draw line to a point that is to the "left" of "c" and is located at "height"
+Draw line back to x,y
+
+(greater than 90)
+Start at x,y
+Move along the x-axis to the length of side-c (i.e. 100 in our example)
+Draw a line to a point that is the "right" of "c" and is located at "height"
+Draw line back to x,y
+
+* Triangle/sss -> a conceptual definition
+* Triangle/ssa -> a conceptual definition
+(...and so on...)
+
+
+
+
 // These two functions represent the first draft of a workable
 // triangle/sas function for Whalesong. They need to be cleaned
 // up and tested, but the basic logic seems to work. 
@@ -19,10 +53,18 @@ function drawLessThan90(){
           // works for <90
 
           ctx.beginPath();
+            if(this.angle < 90) {
           ctx.moveTo(x,y);
           ctx.lineTo(c+x,y);
           ctx.lineTo((c-width)+x,height+y);
           ctx.lineTo(x,y);
+          } else {
+          
+          ctx.moveTo(x,y);
+          ctx.lineTo(c+x,y);
+          ctx.lineTo((c+width)+x,height+y);
+          ctx.lineTo(x,y);
+          }
           ctx.fill();
         }
 
@@ -39,6 +81,8 @@ function drawLessThan90(){
           var a = 100;
           var b = 150;
           var c = 50;
+          var x-radians = x-degrees * Math.PI / 180;
+          var x-degrees = x-radians * 180 / Math.PI;
           var radians = b * 0.0174532952;
           var height = a * Math.sin(radians);
           var width = Math.sqrt((a * a) - (height * height));
